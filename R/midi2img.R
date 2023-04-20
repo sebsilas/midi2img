58aa6bbdd51b7db0db3312ca8b55148ce81ad309
sebsilas <sebsilas@gmail.com>
2023-04-20 19:18:47 +0200

    Update how MuseScore location specified

--- R/midi2img.R
+++ R/midi2img.R
@@ -4,17 +4,18 @@
 
 #' Convert a midi file to an image of its sheet music
 #'
+#' @param mscore_location
 #' @param f
 #'
 #' @return
 #' @export
 #'
 #' @examples
-midi2img <- function(f) {
+midi2img <- function(mscore_location = '/Applications/MuseScore 3.app/Contents/MacOS/mscore', f) {
 
   png_name <- paste0(tools::file_path_sans_ext(f), '.png')
 
-  system2(Sys.getenv("MUSESCORE_LOCATION"),
+  system2(mscore_location,
           args = c(paste0('-o ', png_name),
                    '--trim-image 0',
                    f))
