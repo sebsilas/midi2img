cdbeac57d97c4a094df0b99109517cb6bffe1124
sebsilas <sebsilas@gmail.com>
2023-04-20 19:06:02 +0200

    Documentation

--- R/midi2img.R
+++ R/midi2img.R
@@ -1,10 +1,20 @@
 
 
+
+
+#' Convert a midi file to an image of its sheet music
+#'
+#' @param f
+#'
+#' @return
+#' @export
+#'
+#' @examples
 midi2img <- function(f) {
 
   png_name <- paste0(tools::file_path_sans_ext(f), '.png')
 
-  system2("/Applications/MuseScore 3.app/Contents/MacOS/mscore",
+  system2(Sys.getenv("MUSESCORE_LOCATION"),
           args = c(paste0('-o ', png_name),
                    '--trim-image 0',
                    f))
---
+++ README.md
@@ -0,0 +1,5 @@
+
+
+A simple helper wrapper to convert .mid files to images e.g., for use in publications.
+
+Dependencies: Install the free MuseScore application.
